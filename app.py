10a00fc0d4a42e7f9cf3aed605138ba657ccb764
Nitchanun041 <6531501041@lamduan.mfu.ac.th>
2025-04-20 17:01:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.
 y_kmeans = loaded_model.predict(X)
 
 #plotting
-fig, ax = plt.subplot()
+fig, ax = plt.subplots()
 scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
 ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red')
 ax.set_title('k-Means Clustering')
